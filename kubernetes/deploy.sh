ec509aa81711d5621905693d5123b487193a12ee
Erez Freiberger <erez@epsagon.com>
2020-06-18 17:26:52 +0300

    update deploy script to recieve kubectl flags

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,6 +1,36 @@
 export IMAGE_REPO ?= epsagon
 export IMAGE_NAME ?= auto-inst-mutation-controller
-kubectl create ns epsagon-mutation
+
+export KUBECTL="kubectl"
+
+usage() {
+    cat <<EOF
+Deploy Epsagon Mutation Controller
+
+This deploys 
+EOF
+    exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    case ${1} in
+        --context)
+            export KUBECTL="${KUBECTL} --context ${2}"
+            shift
+            ;;
+        --kubeconfig)
+            export KUBECTL="${KUBECTL} --kubeconfig ${2}"
+            shift
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+
+${KUBECTL} create ns epsagon-mutation
 cat webhook_template.yaml | ./webhook-patch-ca-bundle.sh > webhook_deployment.yaml
 ./webhook-create-signed-cert.sh
-kubectl apply -f webhook_deployment.yaml
+${KUBECTL} apply -f webhook_deployment.yaml
