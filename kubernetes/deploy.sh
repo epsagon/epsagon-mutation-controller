92f4c5081e271930c4c2c8355f24d1018b884f7e
Erez Freiberger <erez@epsagon.com>
2020-08-19 15:28:19 +0300

    refactor deploy script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -5,8 +5,6 @@ if [ -z $IMAGE_NAME ] ; then
     export IMAGE_NAME=auto-inst-mutation-controller
 fi
 
-export KUBECTL="kubectl"
-
 usage() {
     cat <<EOF
 Deploy Epsagon Mutation Controller
@@ -16,29 +14,39 @@ EOF
     exit 1
 }
 
-while [[ $# -gt 0 ]]; do
-    case ${1} in
-        --context)
-            export KUBECTL="${KUBECTL} --context ${2}"
-            shift
-            ;;
-        --kubeconfig)
-            export KUBECTL="${KUBECTL} --kubeconfig ${2}"
-            shift
-            ;;
-        --token)
-            export EPSAGON_TOKEN=${2}
-            shift
-            ;;
-        *)
-            usage
-            ;;
-    esac
-    shift
-done
+function deploy {
+    KUBECTL=$1
+    EPSAGON_TOKEN=$2
+    ${KUBECTL} create ns epsagon-mutation
+    ./webhook-patch-ca-bundle.sh
+    ./webhook-create-signed-cert.sh
+    ${KUBECTL} apply -f webhook_deployment.yaml
+}
 
+function main {
+    KUBECTL="kubectl"
+    while [[ $# -gt 0 ]]; do
+        case ${1} in
+            --context)
+                KUBECTL="${KUBECTL} --context ${2}"
+                shift
+                ;;
+            --kubeconfig)
+                KUBECTL="${KUBECTL} --kubeconfig ${2}"
+                shift
+                ;;
+            --token)
+                EPSAGON_TOKEN=${2}
+                shift
+                ;;
+            *)
+                usage
+                return 1
+                ;;
+        esac
+        shift
+    done
+    deploy $KUBECTL $EPSAGON_TOKEN
+}
 
-${KUBECTL} create ns epsagon-mutation
-./webhook-patch-ca-bundle.sh
-./webhook-create-signed-cert.sh
-${KUBECTL} apply -f webhook_deployment.yaml
+main
